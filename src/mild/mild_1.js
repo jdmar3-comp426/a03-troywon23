6da291c410a8b71088ceb4333df926f4eab99bfa
troywon23 <troywon2@gmail.com>
2021-11-14 10:28:35 -0500

    mild_1 completed

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -43,7 +43,7 @@ export function getIncreasingArray(startNumber, endNumber) {
  * and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
  */
 export function maxAndMin(numbers) {
-    let max_min = {min: Math.min(...numbers), max: Math.max(...numbers)}
+    let max_min = {max: Math.max(...numbers), min: Math.min(...numbers)}
     return max_min;
 }
 
